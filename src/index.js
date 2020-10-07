fd1ab2d7ef0858e1817cc111d23a63da52f611c3
DmitryAstapenko <dmitryastapenko1@gmail.com>
2020-10-07 08:44:20 +0300

    feat: implement check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];  
+  let open = bracketsConfig.map(item => item[0]);
+  let close = bracketsConfig.map(item => item[1]);
+  
+  str.split('').forEach(item => {
+    if (open.includes(item) && close.includes(item)) {
+      if (stack.length && stack[stack.length - 1] === item) {
+        stack.pop();
+      } else {
+        stack.push(item);
+      }      
+    } else if (open.includes(item)) {
+      stack.push(item);
+    } else if (stack.length && open.indexOf(stack[stack.length - 1]) === close.indexOf(item)) {
+      stack.pop();
+    } else {
+      stack.push(item);
+    }    
+  }); 
+
+  return stack.length === 0;
 }
